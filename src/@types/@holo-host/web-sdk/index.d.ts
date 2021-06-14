cc99cce3e85cdfb12b4c78c750a9de09843d952b
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-06-14 13:22:56 +0200

    Updates

---
+++ src/@types/@holo-host/web-sdk/index.d.ts
@@ -0,0 +1,26 @@
+declare module "@holo-host/web-sdk" {
+    import type { AppInfoResponse, AppSignalCb } from "@holochain/conductor-api";
+  
+    type Branding = {
+      app_name: string;
+      logo_url?: string;
+      info_link?: string;
+      publisher_name?: string;
+    };
+  
+    class Connection {
+      chaperone_url: string;
+      constructor(url: string, signalHandler: AppSignalCb, branding: Branding);
+      ready(): Promise<void>;
+      signIn(): Promise<void>;
+      zomeCall(
+        dna_handle: string,
+        zome_name: string,
+        fn_name: string,
+        args: any
+      ): Promise<any>;
+      appInfo(installed_app_id: string): Promise<AppInfoResponse>;
+    }
+    export { Connection, Branding };
+  }
+  
